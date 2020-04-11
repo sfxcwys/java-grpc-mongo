1ba39dcabecc3a3b4372242690b45ba4544541a6
sfxcwys <sfxcwys@gmail.com>
2020-04-11 14:01:22 -0400

    Created boilerplate code for server, client and service

---
+++ src/main/java/com/github/sfxcwys/energy/client/EnergyClient.java
@@ -0,0 +1,14 @@
+package com.github.sfxcwys.energy.client;
+
+public class EnergyClient {
+    public static void main(String[] args) {
+        System.out.println("Hello I'm a gRPC client for Energy");
+
+        EnergyClient main = new EnergyClient();
+        main.run();
+    }
+
+    private void run() {
+
+    }
+}
---
+++ src/main/java/com/github/sfxcwys/energy/server/EnergyServiceImpl.java
@@ -0,0 +1,6 @@
+package com.github.sfxcwys.energy.server;
+
+import com.proto.energy.EnergyServiceGrpc;
+
+public class EnergyServiceImpl extends EnergyServiceGrpc.EnergyServiceImplBase {
+}
