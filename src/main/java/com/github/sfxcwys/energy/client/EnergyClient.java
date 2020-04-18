09d53c958d7a3a6dfc65c7ede1289dd4ab4ebabb
sfxcwys <sfxcwys@gmail.com>
2020-04-18 11:24:43 -0400

    Implemented Client and Server side of StoreEnergy method

--- src/main/java/com/github/sfxcwys/energy/client/EnergyClient.java
+++ src/main/java/com/github/sfxcwys/energy/client/EnergyClient.java
@@ -1,14 +1,70 @@
 package com.github.sfxcwys.energy.client;
 
+import com.proto.energy.EnergyData;
+import com.proto.energy.EnergyServiceGrpc;
+import com.proto.energy.StoreEnergyRequest;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class EnergyClient {
-    public static void main(String[] args) {
+    private Random randomGenerator = new Random();
+    List<Integer> yearList = IntStream.rangeClosed(2019, 2020).boxed().collect(Collectors.toList());
+    List<Integer> monthList = IntStream.rangeClosed(1, 12).boxed().collect(Collectors.toList());
+    List<Integer> dayList = IntStream.rangeClosed(1, 30).boxed().collect(Collectors.toList());
+    List<Integer> hourList = IntStream.rangeClosed(0, 23).boxed().collect(Collectors.toList());
+    List<Integer> minuteList = IntStream.rangeClosed(0, 59).boxed().collect(Collectors.toList());
+    List<Integer> secondList = IntStream.rangeClosed(0, 59).boxed().collect(Collectors.toList());
+    int spaceshipId = 11;
+
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello I'm a gRPC client for Energy");
 
         EnergyClient main = new EnergyClient();
         main.run();
     }
 
-    private void run() {
+    private void run() throws InterruptedException {
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50051)
+                .usePlaintext()
+                .build();
+        EnergyServiceGrpc.EnergyServiceBlockingStub energyClient = EnergyServiceGrpc.newBlockingStub(channel);
 
+        for (int i = 0; i < 5; i++) {
+            List<EnergyData> energyData = generateEnergyData();
+            StoreEnergyRequest storeEnergyRequest = StoreEnergyRequest.newBuilder()
+                    .addAllData(energyData)
+                    .setSpaceshipId(spaceshipId)
+                    .build();
+            energyClient.storeEnergy(storeEnergyRequest);
+            System.out.println(String.format("Inserted data for spaceshipId %d with %d energyData", spaceshipId,
+                    energyData.size()));
+            TimeUnit.SECONDS.sleep(1);
+        }
+    }
+
+    private List<EnergyData> generateEnergyData() {
+        List<EnergyData> result = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int year = yearList.get(randomGenerator.nextInt(yearList.size()));
+            int month = monthList.get(randomGenerator.nextInt(monthList.size()));
+            int day = dayList.get(randomGenerator.nextInt(dayList.size()));
+            int hour = hourList.get(randomGenerator.nextInt(hourList.size()));
+            int minute = minuteList.get(randomGenerator.nextInt(minuteList.size()));
+            int second = secondList.get(randomGenerator.nextInt(secondList.size()));
+            int value = randomGenerator.nextInt(10) + 1;
+            String datetime = String.format("%d-%d-%dT%d:%d:%d", year, month, day, hour, minute, second);
+            result.add(EnergyData.newBuilder()
+                    .setDatetime(datetime)
+                    .setValue(value)
+                    .build());
+        }
+        return result;
     }
 }
--- src/main/java/com/github/sfxcwys/energy/server/EnergyServiceImpl.java
+++ src/main/java/com/github/sfxcwys/energy/server/EnergyServiceImpl.java
@@ -4,11 +4,15 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.proto.energy.EnergyData;
 import com.proto.energy.EnergyServiceGrpc;
 import com.proto.energy.StoreEnergyRequest;
 import com.proto.energy.StoreEnergyResponse;
 import io.grpc.stub.StreamObserver;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class EnergyServiceImpl extends EnergyServiceGrpc.EnergyServiceImplBase {
 
@@ -18,6 +22,26 @@ public class EnergyServiceImpl extends EnergyServiceGrpc.EnergyServiceImplBase {
 
     @Override
     public void storeEnergy(StoreEnergyRequest request, StreamObserver<StoreEnergyResponse> responseObserver) {
-        super.storeEnergy(request, responseObserver);
+        List<Document> docsToInsert = new ArrayList<>();
+        StoreEnergyResponse.Builder storeEnergyResponseBuilder = StoreEnergyResponse.newBuilder();
+
+        for (EnergyData energyData : request.getDataList()) {
+            Document eachDoc = new Document("spaceship_id", request.getSpaceshipId()).append("datetime",
+                    energyData.getDatetime())
+                    .append("value", energyData.getValue());
+            docsToInsert.add(eachDoc);
+        }
+
+        try {
+            collection.insertMany(docsToInsert);
+            System.out.println(String.format("Successfully inserted %d records", docsToInsert.size()));
+            storeEnergyResponseBuilder.setStatus(StoreEnergyResponse.Status.SUCCESS);
+        } catch (Exception e) {
+            System.out.println("Error when inserting into Mongo. Reason: " + e.getMessage());
+            storeEnergyResponseBuilder.setStatus(StoreEnergyResponse.Status.FAILED);
+        }
+
+        responseObserver.onNext(storeEnergyResponseBuilder.build());
+        responseObserver.onCompleted();
     }
 }
